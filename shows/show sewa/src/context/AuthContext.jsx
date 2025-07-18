88344379fd76b1f426c463b10b214d8ad88c8419
kshitiz-12 <kshitiz.vishwakarma2003@gmail.com>
2025-07-19 02:34:39 +0530

    final

--- shows/show sewa/src/context/AuthContext.jsx
+++ shows/show sewa/src/context/AuthContext.jsx
@@ -39,7 +39,8 @@ export const AuthProvider = ({ children }) => {
 
   // Register function
   const register = async (name, email, password) => {
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const res = await fetch(`${apiUrl}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, password }),
